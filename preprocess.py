31483fca624b3805b4c2668708a8d93eb95481df
Jenny.shin <ann4622.sin@gmail.com>
2024-04-03 02:15:59 +0000

    Personal : edit personal path code
    just load path not using config file

--- preprocess.py
+++ preprocess.py
@@ -8,8 +8,9 @@ from preprocessor.preprocessor import Preprocessor
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("config", type=str, help="path to preprocess.yaml")
-    args = parser.parse_args()
-
-    config = yaml.load(open(args.config, "r"), Loader=yaml.FullLoader)
+    # args = parser.parse_args()
+    path = "config/Chem/preprocess.yaml"
+    config = yaml.load(open(path, "r"), Loader=yaml.FullLoader)
     preprocessor = Preprocessor(config)
     preprocessor.build_from_path()
+    
